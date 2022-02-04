92b34b78e8651cd1a5235b538d98b0870d668a11
Prathamesh-Mone <muprathamesh@gmail.com>
2022-02-04 11:24:37 +0530

    Script for setup

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo mkdir /opt/lampp/htdocs/OnlineAirlineManagementSystem
+sudo cp -r ./flights /opt/lampp/htdocs/OnlineAirlineManagementSystem/
+sudo cp -r ./passenger /opt/lampp/htdocs/OnlineAirlineManagementSystem/
+sudo cp -r ./database1.sql /opt/lampp/htdocs/OnlineAirlineManagementSystem/
+sudo cp -r ./firstpage.html /opt/lampp/htdocs/OnlineAirlineManagementSystem/
+sudo cp -r ./flight.jpg /opt/lampp/htdocs/OnlineAirlineManagementSystem/
